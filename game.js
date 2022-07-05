9f6588b5fa4d327dbe4a3d4ca2583dafd3fec23f
Yosef Gehasy <djedjehasi@gmail.com>
2022-07-05 13:46:14 +0300

    struggles with grid layout

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('.container');
 
 
-function cubes(){
+function cubes(n){
     const div = [];
-    for(let i = 0; i < 256; i++){
+    for(let i = 0; i < n; i++){
         div[i] = document.createElement('div');
         div[i].classList.add('cube');
         div[i].setAttribute('id', `${i}`);
@@ -18,24 +18,18 @@ function cubes(){
     });
 }
 
+let grid = 256;
 
-cubes();
-
-
-/*buildGrid();
-
-
-const button = document.querySelector('button');
-
-function buildGrid(){
-
+function buildGrild(){
     const button = document.querySelector('button');
-    grid = button.addEventListener('click', () => {
-        prompt("enter number of grid between 2-100:");
-    });
 
-    grid = grid * grid;
+    button.addEventListener('click', () => {
+        grid = prompt('enter number:');
+        grid = grid * grid;
+        return grid
+    })
+
 }
 
-
-*/
+cubes(grid);
+buildGrild();
