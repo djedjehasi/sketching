611f758a5a0bb1b695e161973628fcd50346f835
Yosef Gehasy <djedjehasi@gmail.com>
2022-07-05 13:08:17 +0300

    creat grid 16X16 + hover futuer

--- game.js
+++ game.js
@@ -1,7 +1,41 @@
-const container = document.querySelector('container');
+const container = document.querySelector('.container');
 
-for(let i = 0; i < 16; i++){
-    document.createElement('div'); 
+
+function cubes(){
+    const div = [];
+    for(let i = 0; i < 256; i++){
+        div[i] = document.createElement('div');
+        div[i].classList.add('cube');
+        div[i].setAttribute('id', `${i}`);
+        container.appendChild(div[i]);
+    }
+    const divCubes = document.querySelectorAll('.cube');
+
+    divCubes.forEach((div) =>{
+        div.addEventListener('mouseover', () => {
+            div.classList.add('hover')
+        });
+    });
 }
 
-const divs = document.querySelectorAll('div');
+
+cubes();
+
+
+/*buildGrid();
+
+
+const button = document.querySelector('button');
+
+function buildGrid(){
+
+    const button = document.querySelector('button');
+    grid = button.addEventListener('click', () => {
+        prompt("enter number of grid between 2-100:");
+    });
+
+    grid = grid * grid;
+}
+
+
+*/
